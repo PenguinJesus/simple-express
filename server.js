7ff8e6e509bfa2601f536e466af3c2608ab00270
PenguinJesus <67099286+PenguinJesus@users.noreply.github.com>
2021-04-03 19:55:07 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+//constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(PORT,HOST);
+console.log(`Running on http://${HOST}:${PORT}`)
